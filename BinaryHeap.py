ca6ca9181ec9f3b4bd9fe3f9c381f5bc3f09a9af
David Ilizarov <dilizarov@gmail.com>
2013-10-26 04:41:20 -0500

    Binary Heap Implementation. Very cool how if you just call delMax n times, it sorts array.

---
+++ BinaryHeap.py
@@ -0,0 +1,46 @@
+class BinaryHeap(object):
+    
+    def __init__(self):
+        self.pq = [None]
+        self.n = 0 #n = no. of elements
+        
+    def isEmpty(self):
+        return self.n == 0
+    
+    def insert(self, key):
+        self.n += 1
+        self.pq.insert(self.n, key)
+        self.swim(self.n)
+        
+    def less(self, i, j):
+        return self.pq[i] < self.pq[j]
+        
+    def exch(self, i, j):
+        self.pq[i], self.pq[j] = self.pq[j], self.pq[i]
+    
+    def swim(self, k):
+        while k > 1 and self.less(k//2, k):
+            self.exch(k, k//2)
+            k //= 2
+    
+    def sink(self, k):
+        while 2*k <= self.n:
+            j = 2*k
+            if j < self.n and self.less(j, j+1):
+                j += 1
+            if not self.less(k, j): 
+                break
+            
+            self.exch(k, j)
+            k = j
+    
+    def delMax(self):
+        try:
+            if self.isEmpty(): raise IndexError
+            max = self.pq[1]
+            self.exch(1, self.n)
+            self.n -= 1
+            self.sink(1)
+            return max
+        except IndexError:
+            print("The Priority Queue is empty! There is no max!")
